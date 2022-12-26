de6f9c1ed60687c1751c4bac7e10b9acb845789c
siddarthreddygsr <siddarthreddygsr@gmail.com>
2022-12-26 15:35:47 +0530

    fixed navigation menu

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ const handleMove = e => {
 document.onmousemove = e => handleMove(e);
 
 document.ontouchmove = e => handleMove(e.touches[0]);
+
+$(function() {
+  function slideMenu() {
+    var activeState = $("#menu-container .menu-list").hasClass("active");
+    $("#menu-container .menu-list").animate({left: activeState ? "0%" : "-100%"}, 400);
+  }
+  $("#menu-wrapper").click(function(event) {
+    event.stopPropagation();
+    $("#hamburger-menu").toggleClass("open");
+    $("#menu-container .menu-list").toggleClass("active");
+    slideMenu();
+
+    $("body").toggleClass("overflow-hidden");
+  });
+
+});
